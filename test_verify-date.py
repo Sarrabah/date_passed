f78da925aee910d18d1067959a1f848b77b975f4
sarrabah <sarrabenarbia1996@gmail.com>
2024-04-04 16:19:49 +0200

    test: Add the new implementation of the method get_current_date

--- test_verify-date.py
+++ test_verify-date.py
@@ -1,8 +1,13 @@
 import pytest
-from verify_date import dateVerifier, Date
+from verify_date import dateVerifier, DateServiceInterface
+
+class TestDateService(DateServiceInterface):
+    def get_current_date(self):
+        current_date = [2024,4,4]
+        return current_date
 
 def test_if_the_date_is_passed_or_not() :
-    current_date = Date()
+    current_date = TestDateService()
     date_verifier= dateVerifier(current_date)
     assert date_verifier.is_past("2024/04/23") == False
 
